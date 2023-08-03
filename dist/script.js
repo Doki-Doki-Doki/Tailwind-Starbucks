c138a6fd45eab1ecc91d8fc03b1b05903076440e
unknown <ryle_andrei_cabudol@dlsl.edu.ph>
2023-08-04 00:33:34 +0800

    late night coding

--- dist/script.js
+++ dist/script.js
@@ -3,5 +3,6 @@ const toggleMenu = document.querySelector(".toggle__menu");
 
 toggleMenu.addEventListener('click', () =>{
     headerNav.classList.toggle('open');
+    toggleMenu.classList.toggle('open');
 })
 
