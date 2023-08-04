ac6ccfc4d104c651a978f95b43a488d2a2fc8f43
LegendaryPaul <paulanjelozzz@gmail.com>
2023-08-04 15:43:39 +0800

    faves in progress

--- dist/script.js
+++ dist/script.js
@@ -1,8 +1,12 @@
 const headerNav = document.querySelector(".header__nav");
 const toggleMenu = document.querySelector(".toggle__menu");
+const span = document.querySelector("#test");
 
 toggleMenu.addEventListener('click', () =>{
     headerNav.classList.toggle('open');
     toggleMenu.classList.toggle('open');
 })
 
+toggleMenu.addEventListener('mouseover', () =>{
+    span.classList.toggle('open');
+})
