24657ed0383f8a215dd590280e320dedbd9eda46
LegendaryPaul <paulanjelozzz@gmail.com>
2023-08-04 15:51:07 +0800

    to do at home

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'puti':'#ffffff',
+            'maitim':'#2b2b2b',
+            'lasalle':'#006341',
+            'gento':'#006341',
+            'semento':'hsl(240,1.51%,50.98%)',
+            'BG':'#d4e9e2',
+            'get':'#F1F8F5',
+      }
+    },
   },
   plugins: [],
 }
