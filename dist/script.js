f0b964e9b5bb704c5ed0ce60498de9786b5cfcff
unknown <ryle_andrei_cabudol@dlsl.edu.ph>
2023-08-06 01:04:22 +0800

    needs few more touches

--- dist/script.js
+++ dist/script.js
@@ -1,28 +1,56 @@
-const kalboNav = document.querySelectorAll(".kalbo__nav__link");
-const kalboContent = document.querySelectorAll(".kalbo__item__content");
-kalboNav.forEach((kalbo) => {
-    kalbo.addEventListener("click", () => {
-    removeActiveKalbo();
-    kalbo.style = "transition:.5s ease";
-    kalbo.classList.add("active");
-    kalbo.style = "transition:.5s ease";
-    const activeContent = document.querySelector(`#${kalbo.id}-content`);
-    removeActiveContent();
+// const kalboNav = document.querySelectorAll(".kalbo__nav__link");
+// const kalboContent = document.querySelectorAll(".kalbo__item__content");
+// kalboNav.forEach((kalbo) => {
+//     kalbo.addEventListener("click", () => {
+//     removeActiveKalbo();
+//     kalbo.style = "transition:.5s ease";
+//     kalbo.classList.add("active");
+//     kalbo.style = "transition:.5s ease";
+//     const activeContent = document.querySelector(`#${kalbo.id}-content`);
+//     removeActiveContent();
+//     activeContent.classList.add("active");
+//     });
+// });
+
+// function removeActiveKalbo() {
+//     kalboNav.forEach((kalbo) => {
+//     kalbo.classList.remove("active");
+//     });
+// }
+
+// function removeActiveContent() {
+//     kalboContent.forEach((kalbo) => {
+//     kalbo.classList.remove("active");
+//     });
+// }
+
+
+const getNav = document.querySelectorAll(".get__nav__link");
+const getContent = document.querySelectorAll(".get__item__content");
+getNav.forEach((get) => {
+    get.addEventListener("click", () => {
+    removeActiveGet();
+    get.style = "transition:.5s ease";
+    get.classList.add("active");
+    get.style = "transition:.5s ease";
+    const activeContent = document.querySelector(`#${get.id}-content`);
+    removeActiveGetContent();
     activeContent.classList.add("active");
     });
 });
 
-function removeActiveKalbo() {
-    kalboNav.forEach((kalbo) => {
-    kalbo.classList.remove("active");
+function removeActiveGet() {
+    getNav.forEach((get) => {
+    get.classList.remove("active");
     });
 }
 
-function removeActiveContent() {
-    kalboContent.forEach((kalbo) => {
-    kalbo.classList.remove("active");
+function removeActiveGetContent() {
+    getContent.forEach((get) => {
+    get.classList.remove("active");
     });
 }
+
 
 
 const headerNav = document.querySelector(".header__nav");
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,12 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        'puti':'#ffffff',
-            'maitim':'#2b2b2b',
-            'lasalle':'#006341',
-            'gento':'#006341',
-            'semento':'hsl(240,1.51%,50.98%)',
-            'BG':'#d4e9e2',
-            'get':'#F1F8F5',
+          'puti':'#ffffff',
+          'maitim':'#2b2b2b',
+          'lasalle':'#006341',
+          'semento':'hsl(240,1.51%,50.98%)',
+          'lberde':'#d4e9e2',
+          'vlberde':'#F1F8F5',
       }
     },
   },
