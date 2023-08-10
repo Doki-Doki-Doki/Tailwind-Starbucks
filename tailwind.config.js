cf73342a93bc9ec16f2c1976bf2932f8f029e25b
LegendaryPaul <paulanjelozzz@gmail.com>
2023-08-10 13:21:38 +0800

    starbs home page done

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,16 @@ module.exports = {
           'puti':'#ffffff',
           'itim':'#000000',
           'maitim':'#2b2b2b',
-          'lasalle':'#006341',
+          'lasalle':'#006241',
           'semento':'hsl(240,1.51%,50.98%)',
           'lberde':'#d4e9e2',
           'vlberde':'#F1F8F5',
           'diputi':'#f2f0eb',
           'fputi':'#f9f9f9',
+          'penk': '#EB81A5',
+          'tlasalle': '#1e3932',
+          'periwinks': '#8f8bf4',
+          'sbyellow': '#f1ff67'
       },
       backgroundImage:{
         'star':"url('../img/rewards/star.png')",
